d810ecd9f0ab6c85ca0ae58d65a18c4b762a954e
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-06-08 18:58:45 +0200

    Implement FAT file offset parsing from blocks

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', async () => {
   // https://download.bbbike.org/osm/bbbike/Prag/
-  // http://mirror74.boot-keys.org/Soft/Map/GPS/gpsVP/svn_v375/doc/imgformat.pdf
+  // https://github.com/jerome077/gpsvp/blob/master/doc/imgformat.pdf
   const response = await fetch('Prag.img');
   const arrayBuffer = await response.arrayBuffer();
   const dataView = new DataView(arrayBuffer);
-  document.body.textContent = JSON.stringify(new GarminImg(dataView).subfiles, null, 2);
+  const garminImg = new GarminImg(dataView);
+  document.body.textContent = JSON.stringify(garminImg.subfiles, null, 2);
 });
 
 class GarminImg {
@@ -42,29 +43,126 @@ class GarminImg {
     this.mapDescription = String.fromCharCode(...new Uint8Array(dataView.buffer.slice(0x49, 0x5d)));
     this.mapName = String.fromCharCode(...new Uint8Array(dataView.buffer.slice(0x65, 0x83 /* Drop \0 terminator */))).trim();
 
-    // TODO: Figure out how to calculate these (the FAT?)
-    const handFoundOffsets = [19456, /* RGN */ 89600, /* TRE */ 90624, /* LBL */ 93229, /* LTD */ 93295, /* LTD */ 93311, /* IMA */ 102912, /* NET */ 197120, /* NOD */ 391168, /* RGN */ 489472, /* TRE */ 490496, /* LBL */ 494571, /* LTD */ 494637, /* LTD */ 494653, /* IMA */ 508416, /* NET */ 627200, /* NOD */ 882688, /* RGN */ 976896, /* TRE */ 977920, /* LBL */ 981385, /* LTD */ 981451, /* LTD */ 981467, /* IMA */ 993280, /* NET */ 1098752, /* NOD */ 1322496, /* RGN */ 1395712, /* TRE */ 1396736, /* LBL */ 1399165, /* LTD */ 1399231, /* LTD */ 1399247, /* IMA */ 1408000, /* NET */ 1495552, /* NOD */ 1686016, /* RGN */ 1780736, /* TRE */ 1781760, /* LBL */ 1787695, /* LTD */ 1787761, /* LTD */ 1787777, /* IMA */ 1801216, /* NET */ 1914368, /* NOD */ 2152960, /* MDR */ 2175488, /* SRT */];
-
     const firstSubfileOffset = dataView.getUint32(0x40c, true);
-    if (firstSubfileOffset !== handFoundOffsets[0]) {
-      throw new Error(`The first subfile offset doesn't match anymore.`);
+
+    const fatOffset = 0x600;
+    const fatLength = firstSubfileOffset - fatOffset;
+    const blockSize = 512;
+    const blockCount = fatLength / blockSize;
+
+    const files = [];
+
+    for (let blockIndex = 0; blockIndex < blockCount; blockIndex++) {
+      const blockDataView = new DataView(dataView.buffer, fatOffset + blockIndex * 512);
+      const blockFlag = blockDataView.getUint8(0x0);
+      if (blockFlag === 0x0) {
+        if (blockIndex !== blockCount - 1) {
+          throw new Error('Dummy block found but is not last block!');
+        }
+
+        break;
+      }
+
+      const blockSubfileName = String.fromCharCode(...new Uint8Array(blockDataView.buffer.slice(blockDataView.byteOffset + 0x1, blockDataView.byteOffset + 0x9)));
+      const blockSubfileType = String.fromCharCode(...new Uint8Array(blockDataView.buffer.slice(blockDataView.byteOffset + 0x9, blockDataView.byteOffset + 0xc)));
+      const blockSubfileSize = blockDataView.getUint16(0xc, true); // If blockSubfilePart === 0x0
+      const blockSubfilePart = blockDataView.getUint16(0x10, true);
+
+      let file;
+      if (blockSubfilePart === 0) {
+        file = files.find(f => f.name === blockSubfileName && f.type === blockSubfileType);
+        if (file) {
+          throw new Error('File already exists!');
+        }
+
+        file = { name: blockSubfileName, type: blockSubfileType, size: blockSubfileSize };
+        files.push(file);
+      } else {
+        file = files.find(f => f.name === blockSubfileName && f.type === blockSubfileType);
+        if (!file) {
+          throw new Error('File does not exist!');
+        }
+      }
+
+      const blockSequenceNumbers = blockDataView.buffer.slice(0x20, 0x200);
+      const sequenceNumberCount = blockSequenceNumbers.byteLength / 2;
+      for (let sequenceNumberIndex = 0; sequenceNumberIndex < sequenceNumberCount; sequenceNumberIndex++) {
+        const sequenceNumber = blockDataView.getUint16(0x20 + sequenceNumberIndex * 2 /* 2 bytes in uint16 */, true);
+        if (file.blocks === undefined) {
+          if (sequenceNumber === 0xffff) {
+            throw new Error('No blocks? Is this a zero-sized file?');
+          }
+
+          file.blocks = { sequenceNumber, count: 1, terminated: false };
+        } else if (file.blocks.terminated) {
+          if (sequenceNumber !== 0xffff) {
+            throw new Error('Expected only padding now!');
+          }
+        } else {
+          if (sequenceNumber === 0xffff) {
+            file.blocks.terminated = true;
+            continue;
+          }
+
+          if (file.blocks.count !== sequenceNumber - file.blocks.sequenceNumber) {
+            throw new Error('Numbers are not sequential');
+          }
+
+          file.blocks.count++;
+        }
+      }
+
+      delete file.blocks.terminated;
     }
 
+    // These offsets found using the FAT:
+    // 19456, 89600, 90624, 102912, 197120, 391168, 489472, 490496, 508416, 627200, 882688, 976896, 977920, 993280, 1098752, 1322496, 1395712, 1396736, 1408000, 1495552, 1686016, 1780736, 1781760, 1801216, 1914368, 2152960, 2175488
+    for (const file of files) {
+      file.offset = file.blocks.sequenceNumber * 512;
+    }
+
+    // TODO: Find out why these offsets do not appear in the FAT (these were found by full text search for "GARMIN ")
+    files.push({ offset: 93229, type: 'LTD' });
+    files.push({ offset: 93295, type: 'LTD' });
+    files.push({ offset: 93311, type: 'IMA' });
+    files.push({ offset: 494571, type: 'LTD' });
+    files.push({ offset: 494637, type: 'LTD' });
+    files.push({ offset: 494653, type: 'IMA' });
+    files.push({ offset: 981385, type: 'LTD' });
+    files.push({ offset: 981451, type: 'LTD' });
+    files.push({ offset: 981467, type: 'IMA' });
+    files.push({ offset: 1399165, type: 'LTD' });
+    files.push({ offset: 1399231, type: 'LTD' });
+    files.push({ offset: 1399247, type: 'IMA' });
+    files.push({ offset: 1787695, type: 'LTD' });
+    files.push({ offset: 1787761, type: 'LTD' });
+    files.push({ offset: 1787777, type: 'IMA' });
+
     this.subfiles = [];
-    for (const offset of handFoundOffsets) {
-      const subfileDataView = new DataView(dataView.buffer, offset);
-      const type = String.fromCharCode(...new Uint8Array(subfileDataView.buffer.slice(offset + 0x2, offset + 0xc)));
-      switch (type) {
-        case 'GARMIN RGN': this.subfiles.push(new GarminRgnSubfile(subfileDataView)); break;
-        case 'GARMIN TRE': this.subfiles.push(new GarminTreSubfile(subfileDataView)); break;
-        case 'GARMIN LBL': this.subfiles.push(new GarminTreSubfile(subfileDataView)); break;
-        case 'GARMIN NET': this.subfiles.push(new GarminNetSubfile(subfileDataView)); break;
-        case 'GARMIN NOD': this.subfiles.push(new GarminNodSubfile(subfileDataView)); break;
-        case 'GARMIN MDR': this.subfiles.push(new GarminMdrSubfile(subfileDataView)); break;
-        case 'GARMIN SRT': this.subfiles.push(new GarminSrtSubfile(subfileDataView)); break;
-        case 'GARMIN LTD': this.subfiles.push(new GarminLtdSubfile(subfileDataView)); break;
-        case 'GARMIN IMA': this.subfiles.push(new GarminImaSubfile(subfileDataView)); break;
-        default: throw new Error(`Unpexpected subfile type: '${type}' at offset ${offset}.`);
+    for (const file of files) {
+      // TODO: Figure this file out
+      if (file.name === 'MAKEGMAP') {
+        continue;
+      }
+
+      // TODO: Limit the data view length to `file.blocks.count * 512` when I have `blocks` for the offsets found by full text
+      const subfileDataView = new DataView(dataView.buffer, file.offset);
+      const type = String.fromCharCode(...new Uint8Array(subfileDataView.buffer.slice(file.offset + 0x2, file.offset + 0xc)));
+      if ('GARMIN ' + file.type !== type) {
+        throw new Error(`Type mismatch between FAT (${file.type}) and data (${type})`);
+      }
+
+      switch (file.type) {
+        case 'RGN': this.subfiles.push(new GarminRgnSubfile(subfileDataView)); break;
+        case 'TRE': this.subfiles.push(new GarminTreSubfile(subfileDataView)); break;
+        case 'LBL': this.subfiles.push(new GarminTreSubfile(subfileDataView)); break;
+        case 'NET': this.subfiles.push(new GarminNetSubfile(subfileDataView)); break;
+        case 'NOD': this.subfiles.push(new GarminNodSubfile(subfileDataView)); break;
+        case 'MDR': this.subfiles.push(new GarminMdrSubfile(subfileDataView)); break;
+        case 'SRT': this.subfiles.push(new GarminSrtSubfile(subfileDataView)); break;
+        case 'LTD': this.subfiles.push(new GarminLtdSubfile(subfileDataView)); break;
+        case 'IMA': this.subfiles.push(new GarminImaSubfile(subfileDataView)); break;
+        default: throw new Error(`Unpexpected subfile type: '${file.type}' at offset ${file.offset}.`);
       }
     }
   }
